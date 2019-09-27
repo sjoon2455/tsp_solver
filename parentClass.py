9c3524749ac755a4799d0882c0f38368c6bea222
sjoon2455 <sjoon2455@kaist.ac.kr>
2019-09-27 22:02:04 +0900

    selecting...

--- parentClass.py
+++ parentClass.py
@@ -4,3 +4,6 @@ class Parent:
     def __init__(self, li, fit):
         self.li = li
         self.fit = fit
+    
+    def getFitness(self):
+        return self.fit
--- tsp_solver.py
+++ tsp_solver.py
@@ -3,6 +3,40 @@ import sys
 import random
 from cityClass import City
 from parentClass import Parent
+
+################################################# MAJOR FUNCTION #################################################
+### get initial population using random shuffle. Number of initial population able to assign.
+### input: number of cities, num of initial population
+### output: list of list of int(city)
+def initialPopulation(num, pop):
+    population = []
+    alignedList = list(range(1, num+1))
+    for i in list(range(1, pop+1)):
+        random.shuffle(alignedList)
+        population.append(alignedList)
+    return population
+
+
+### calculate fitness: total distance between cities
+### input: list of int
+### output: int
+def computeFitness(route, cities):
+    distance = 0
+    actualRoute = [cities[i-1] for i in route]
+    length = len(actualRoute)
+    # starts with i=0
+    for i in range(length):
+        if i == (length-1):
+            distance += actualRoute[i].getDistance(actualRoute[0])
+        else:
+            distance += actualRoute[i].getDistance(actualRoute[i+1])
+    return distance
+        
+
+
+
+
+
 ### given list of int with city indices, create csv file
 ### input: list of int
 ### output: .csv with single column city indices
@@ -14,48 +48,20 @@ def createCSV(arg):
         csv.write(data)
     return csv
 
-### get initial population using random shuffle. Number of initial population able to assign.
-### input: number of cities, num of initial population
-### output: list of list of int(city)
-def initialPopulation(num, pop):
-    population = []
-    alignedList = list(range(1, num+1))
-    for i in list(range(1, pop+1)):
-        random.shuffle(alignedList)
-        population.append(alignedList)
-    return population
-#print(initialPopulation(5, 3))
 
-'''
-def initialPopulationMST(num, pop):
-    population = []
-    alignedList = list(range(1, num+1))
-'''
-
-### calculate fitness: total distance between cities
-### input: list of int
+################################################# HELPER FUNCTION #################################################
+### add all fitness values of current population
+### input: list of parents
 ### output: int
-def getFitness(route, cities):
-    distance = 0
-    actualRoute = [cities[i-1] for i in route]
-    length = len(actualRoute)
-    # starts with i=0
-    for i in range(length):
-        if i == (length-1):
-            distance += actualRoute[i].getDistance(actualRoute[0])
-        else:
-            distance += actualRoute[i].getDistance(actualRoute[i+1])
-            
-        
+def sumFitness(parents):
+    res = 0
+    for i in parents:
+        fit = i.getFitness()
+        res += fit
+    return res
 
 
-'''
-def select()
-def getFitness()
-def crossover()
-def mutate()
-'''
-
+################################################# MAIN FUNCTION #################################################
 ### main function
 ### input: -
 ### output: .csv
@@ -73,23 +79,23 @@ def main():
     #cities: list of City()
     cities = []
     for i in range(0, num):
-        x = lines[i][1]
-        y = lines[i][2]
-        cities.append(City(i, x, y))
+        x = int(lines[i][1])
+        y = int(lines[i][2])
+        cities.append(City(i+1, x, y))
     initialPopulation = initialPopulation(num, pop)
     
+    # main loop
     count = 0
     while count < loop:
         count += 1
         parents = []
         #route: [3, 5, 61, 24, 6, ..., 2412]
         for route in initialPopulation:
-            fitness = getFitness(route, cities)
+            fitness = computeFitness(route, cities)
             parent = Parent(route, fitness)
             parents.append()
+        sumFitness = sumFitness(parents)
     
-    
-    #print(lines[:12])
 
 
     
